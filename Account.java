8d7bbf597cb547cad2b286a674a5e77e8676e544
trung2709 <61338358+trung2709@users.noreply.github.com>
2021-03-20 12:28:34 +0700

    add accountjava

---
+++ Account.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+/**
+ *
+ * @author Admin
+ */
+public class Account {
+    private int uID;
+    private String username;
+    private String password;
+    private int isSell;
+    private int isAdmin;
+
+    public Account(int uID, String username, String password, int isSell, int isAdmin) {
+        this.uID = uID;
+        this.username = username;
+        this.password = password;
+        this.isSell = isSell;
+        this.isAdmin = isAdmin;
+    }
+
+    public int getuID() {
+        return uID;
+    }
+
+    public void setuID(int uID) {
+        this.uID = uID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getIsSell() {
+        return isSell;
+    }
+
+    public void setIsSell(int isSell) {
+        this.isSell = isSell;
+    }
+
+    public int getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(int isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+
+    
+    
+    
+}
